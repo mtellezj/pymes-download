c36504fa2b11854241b25cf5a70ddf569d0852b0
Mauricio Tellez <mauricio@solucioni.mx>
2019-05-16 19:47:04 -0500

    Initial commit.

---
+++ pymes-download.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Download data from the site http://pymes.org.mx
+"""
+
+import tqdm
+
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    pass
